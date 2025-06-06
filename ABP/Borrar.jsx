1a36e30647b9876c2c70017198ab639223732349
Valentina Pich <169078817+Vaal-D4t4Sc@users.noreply.github.com>
2025-06-06 14:13:00 -0300

    Update and rename Borrar to Borrar.jsx

---
+++ ABP/Borrar.jsx
@@ -0,0 +1,50 @@
+// npm install tailwindcss @tailwindcss/vite (listo)
+
+// import { defineConfig } from 'vite'
+// import tailwindcss from '@tailwindcss/vite'
+// export default defineConfig({
+//   plugins: [
+    // tailwindcss(),
+  // ],
+// })
+// (listo)
+
+// recomendación: tailblocks.cc
+//sitio online con ejemplos de componentes/ventanas de interfaces compatibles con tailwind
+
+//cambios! class --> className
+//cambios! img se debe cerrar autonoma
+// con la IA de vsc podemos adecuarlo rapido sin problema
+//(listo) 
+
+
+// https://fakestoreapi.com/
+//(listo)
+
+// Node - hace correr java sin necesidad de un navegador
+// npm (node package manager) - gestor de paquetes de node, para lo anterior pero mejor ;)
+
+// Axios - para consumir las APIs
+// tailwind - para el diseño
+// vite - el entorno de desarrollo
+// react - la libreria de java para crear interfaces
+
+
+//en vez de uisar "fetch" usamos "axios.get" para que sea mas sencillo y copiado desde un API fake
+
+//las variables son como estados
+// los efectos es que se ejecutan cuando se renderiza el componente
+
+{/* formato para comentarios en App.jsx */}
+
+
+
+// ETIQUETAS UTILES DE HTML PARA ESTILIZAR EL TEXTO
+
+//https://www.ionos.com/es-us/digitalguide/paginas-web/desarrollo-web/html-tags/
+
+//https://es.semrush.com/blog/lista-de-html-tags/
+
+
+//BLOB
+// abreviaciaion de binary data big object(?). Consultar mdn devoloper docs, sino consultar pasos a IA

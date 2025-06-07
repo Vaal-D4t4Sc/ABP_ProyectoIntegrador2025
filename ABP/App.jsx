6a6f270f49f001ffcbe9ab9330f6241bf4849d47
Valentina Pich <169078817+Vaal-D4t4Sc@users.noreply.github.com>
2025-06-07 00:42:43 -0300

    Update App.jsx

--- ABP/App.jsx
+++ ABP/App.jsx
@@ -1,53 +1,65 @@
 import './App.css'
 import axios from 'axios';
-import {useEffect} from 'react'; // hook
-import {useState} from 'react'; //el hook useEffect permite ejecutar efectos secundarios en componentes funcionales
-import Stats from "./components/Stats";
+import {useEffect, useState, useRef} from 'react'; // hook
+
 
 
 //components
+import Stats from "./components/Stats";
 import Card from "./components/card";
 import Footer from "./components/Footer";
 
 function App() {
 
-    const [recipes, setRecipes] = useState ([]);
+    const [products, setProducts] = useState ([]);
     const [search, setSearch] = useState("");
     const [show, setShow] = useState(true);
-
-    useEffect( ()=>{
-      axios.get(`https://dummyjson.com/recipes?limit=150`)
-        .then((res)=>{setRecipes(res.data.recipes)})
-        .catch((error) => console.error("Error fetching recipes:", error));
-  },[]);
-
-    const filteredRecipes = recipes.filter((p) => p.name.toLowerCase().includes(search.toLowerCase()));
-    //resultados no distinguen entre Mayusculas o Minusculas, se unifica a minuscula
-
-    {/* Total de productos */}
-    const totalRecipes = filteredRecipes.length;
-
+    const [page, setPage] = useState(1);
+    const [darkMode, setDarkMode] = useState(false);
+    /*const [format, setFormat] = useState('');*/
 
     
-    
-    {/* Mayor demora */} {/* los puntos seguidos son Spread Operator */}
-    const maxTime = filteredRecipes.length > 0 ? Math.max(...filteredRecipes.map((p) => p.prepTimeMinutes)) : 0 ;
-    
-    {/* Menor demora */} {/* los puntos seguidos son Spread Operator */}
-    const minTime = filteredRecipes.length > 0 ? Math.min(...filteredRecipes.map((p) => p.prepTimeMinutes)) : 0 ;
-    
-    {/* Promedio de calorias [reveer paso a paso] */}
-    const aveCalories = filteredRecipes.length > 0 
-  ? filteredRecipes.reduce((acc, recipe) => acc + recipe.caloriesPerServing, 0) / filteredRecipes.length
-  : 0;
+    //Referencias
+    const containerRef = useRef (null);
+    const limit = 30
 
+
+  useEffect(() => {
+        axios.get(`https://dummyjson.com/products?limit=${limit}&skip=${(page - 1) * limit}`).then((res) => {
+            setProducts(res.data.products);
+        });
+    }, [page]);
+
+    const filteredProducts = products.filter((p) => p.title.toLowerCase().includes(search.toLowerCase()));
+    {/*resultados no distinguen entre Mayusculas o Minusculas, se unifica a minuscula */}
+
+    {/* Total de productos */} {/* constantes para las estadisticas */}
+    const totalProducts = filteredProducts.length;
+    
+    {/* Mayor Costo */} {/* los puntos seguidos son Spread Operator 
+    const maxPrice = filteredProducts.length > 0 ? Math.max(...filteredProducts.map((p) => p.prepTimeMinutes)) : 0 ;*/}
+    const maxPrice = Math.max(...filteredProducts.map((p) => p.price));
+    
+    {/* Menor demora */} {/* los puntos seguidos son Spread Operator 
+    const minPrice = filteredProducts.length > 0 ? Math.min(...filteredProducts.map((p) => p.prepTimeMinutes)) : 0 ; */}
+    const minPrice = Math.min(...filteredProducts.map((p) => p.price));
+
+    
+    {/* Promedio de calorias [reveer paso a paso] 
+    const avgCalories = 
+        filteredProducts.length > 0
+        ? (filteredProducts.reduce((sum, p) => sum + p.caloriesPerServing, 0) / filteredProducts.length): 0;
+        */}
+
+    const toggleDarkMode = ()=>{
+      setDarkMode(!darkMode);
+      containerRef.current.classList.toggle("dark-mode");   
+    }  
 
     return (
-      <>
-          <div> 
-              <h1>  </h1>    
-          </div>
-
+      <div ref={containerRef}>          
+          <button onClick={toggleDarkMode}> Modo {darkMode ? 'Claro' : 'Oscuro'}</button>
+          
           <h1 className="text-center text-5xl font-bold font-sans">
             Proyecto Integrador 2025 </h1>
 
@@ -56,52 +68,96 @@ function App() {
 
           <h1 className="text-center text-5xl font-bold font-sans">
             Alumna Pich Valentina</h1>
+           
+          <br>
+          </br>  
           
-          <div> 
-              <h1 className= "text-3xl">   </h1>    
-          </div>
+          <br>
+          </br>  
+          
+          <br>
+          </br>  
+          
+          <br>
+          </br>  
 
-          <h2 className="text-left text-2xl font-bold font-sans"> Recetas sabrosas </h2>
+          <h2 className="text-left text-2xl font-bold font-sans"> Encuentra el producto: </h2>
 
-          <Card/>
+          {/*<Card/>*/}
 
           <h3> {search} </h3>
 
+
+
+          <br> 
+          </br> 
+
+            <button 
+            onClick={() => setShow(!show)}>{show ? "Ocultar" : "Mostrar"} </button>
+
+ 
+          <br> 
+          </br> 
           <div>
 
-            <h1 className= "text-3xl"> Estadísticas   </h1>
 
             {/* Renderización condicional */}
-            {show && <Stats total={totalRecipes} max={maxTime} min={minTime} ave={aveCalories} />}
-            {filteredRecipes.length == 0 && <div> Sin resultados coincidentes </div>}
+            {show && <Stats 
+                      total={totalProducts} 
+                      max={maxPrice} 
+                      min={minPrice} 
+                      /* ave={avgCalories} */ />}
+            {filteredProducts.length == 0 && <div> Sin resultados coincidentes </div>}
           </div> 
+
+          <br> 
+          </br>
 
           <input 
               type="text" 
-              placeholder="Buscar receta" 
+              placeholder="Buscar producto" 
               value={search} 
               onChange={(e)=>{setSearch(e.target.value)}}
           />
           
-          <div> 
-              <h1>  </h1>    
-          </div>
+          <br> 
+          </br>
+
+
+          <select>
+            <option> Seleccionar Formato</option>
+            <option value="json"> JSON </option>
+            <option value=""> Otrossssss </option>
+   
+          </select>
+
 
           <ul>
-            {filteredRecipes.map((p)=>(
-              <li key={p.id}> {p.name}, *{p.rating}</li>))}
+            {filteredProducts.map((p)=>(
+              <li key= {p.id}> {p.title}, $ {p.price}</li>))}
           </ul>
+          
+          <br> 
+          </br>
 
-          <div> 
-              <h1>  </h1>    
-          </div>
+          <button disabled = {page === 1}
+                  onClick={()=> {
+                      setPage(page - 1);
+          }} > Página anterior</button>
 
-          <button 
-            onClick={() => setShow(!show)}>{show ? "Ocultar" : "Mostrar"} </button>
+          <button onClick={()=> {
+                      setPage(page + 1);
+          }} > Página siguiente</button>
+
+          <br> 
+          </br>
+
+            <button 
+            onClick={() => setShow(!show)}>{show ? " Ocultar " : " Mostrar "} </button>
 
 
           <Footer />
-      </>
+      </div>
   )
 }
 
